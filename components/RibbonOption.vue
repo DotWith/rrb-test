e3823fc941425e84669f5d7267b367d553c2c84e
DotWith <dotwith@tutanota.com>
2024-08-30 00:29:22 -0400

    init

---
+++ components/RibbonOption.vue
@@ -0,0 +1,15 @@
+<script setup>
+const props = defineProps({
+    label: String,
+    imgSrc: String,
+});
+</script>
+
+<template>
+    <div>
+        <!-- <input type="checkbox" :id="id" :value="value" :checked="isChecked"> -->
+        <img :src="imgSrc">
+        <label>{{ label }}</label>
+        <!-- <input type="range" min="0" max="25" value="0" v-model="devices" @click.stop /> -->
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+const { data } = await useAsyncData('ribbons', () => queryContent('/ribbons_meta').findOne());
+
+function getRibbonSvgPath(id) {
+  return `/ribbons/${id}.svg`;
+}
+</script>
+
+<template>
+    <div>
+        <h1>Welcome to the homepage</h1>
+        <ContentDoc />
+        <div v-for="ribbon in data.body" :key="ribbon.id">
+            <RibbonOption :label="`${ribbon.id}. ${ribbon.name}`" :imgSrc="getRibbonSvgPath(ribbon.id)" />
+            <!-- <img :src="`/ribbons/${ribbon.id}.svg`" /> -->
+        </div>
+        <AppAlert>
+            This is an auto-imported component
+        </AppAlert>
+    </div>
+</template>
